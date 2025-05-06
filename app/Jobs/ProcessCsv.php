dc1d5064f26802133be9a9200ae5243d1e82a239
Marcus loo <Marcuslooek@gmail.com>
2025-05-06 18:57:46 +0800

    solution-4

--- app/Jobs/ProcessCsv.php
+++ app/Jobs/ProcessCsv.php
@@ -8,6 +8,7 @@ use App\Models\FileUpload;
 use App\Models\Product;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -35,26 +36,21 @@ class ProcessCsv implements ShouldQueue
         $productDataChunk = [];
         $handle = fopen(storage_path('app/private/' . $this->fileUpload->file_path), 'r');
         fgetcsv($handle); // skip header
+
+        $pdo = DB::connection()->getPdo();
+        $stmt = $pdo->prepare("UPSERT INTO products (unique_key, product_title, product_description, style, sanmar_mainframe_color, size, color_name, piece_price) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
         while (($row = fgetcsv($handle)) !== false) {
-            $productDataChunk[] = [
-                'unique_key' => mb_convert_encoding($row[0], 'UTF-8'),
-                'product_title' => mb_convert_encoding($row[1], 'UTF-8'),
-                'product_description' => mb_convert_encoding($row[2], 'UTF-8'),
-                'style' => mb_convert_encoding($row[3], 'UTF-8'),
-                'sanmar_mainframe_color' => mb_convert_encoding($row[28], 'UTF-8'),
-                'size' => mb_convert_encoding($row[18], 'UTF-8'),
-                'color_name' => mb_convert_encoding($row[14], 'UTF-8'),
-                'piece_price' => mb_convert_encoding($row[21], 'UTF-8'),
-            ];
-
-            if (count($productDataChunk) === 1000) {
-                Product::upsert($productDataChunk, uniqueBy: ['unique_key'], update: ['product_title', 'product_description', 'style', 'sanmar_mainframe_color', 'size', 'color_name', 'piece_price']);
-                $productDataChunk = [];
-            }
-
-            if (!empty($productDataChunk)) {
-                Product::upsert($productDataChunk, uniqueBy: ['unique_key'], update: ['product_title', 'product_description', 'style', 'sanmar_mainframe_color', 'size', 'color_name', 'piece_price']);
-            }
+            $stmt->execute([
+                mb_convert_encoding($row[0], 'UTF-8'),
+                mb_convert_encoding($row[1], 'UTF-8'),
+                mb_convert_encoding($row[2], 'UTF-8'),
+                mb_convert_encoding($row[3], 'UTF-8'),
+                mb_convert_encoding($row[28], 'UTF-8'),
+                mb_convert_encoding($row[18], 'UTF-8'),
+                mb_convert_encoding($row[14], 'UTF-8'),
+                mb_convert_encoding($row[21], 'UTF-8'),
+            ]);
         }
         fclose($handle);
 
